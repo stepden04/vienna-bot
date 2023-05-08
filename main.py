578db92d272ab57e36c14653adcc1a12cbcf46cb
Stepan Denysenko <stepden04@gmail.com>
2023-05-09 02:31:49 +0300

    TODO download photos, check for new listings added links for photos

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-from telebot import TeleBot
+from telebot import TeleBot, types
 from schedule import every, run_pending,repeat
 from threading import Thread
 from time import sleep
 from dotenv import dotenv_values
+from page import init_titles_file,Listing
+types.Message.parse_photo()
+config = dotenv_values()
 
-API_TOKEN = dotenv_values()['TOKEN']
-CHANNEL_ID = dotenv_values()['CHANNEL']
+bot = TeleBot(config['TOKEN'], parse_mode=None) # bot init
 
-bot = TeleBot(API_TOKEN, parse_mode=None) # bot init
+PREVIOUS_LISTINGS = init_titles_file(config["TITLES_PATH"])
 
-# TODO store in file
-previous_titles = set() # better then list to store unique items
 
 @bot.message_handler(commands=['start', 'help'])
 def send_start(message):
-    bot.reply_to(message, 'cum')
+    try:
+        bot.reply_to(message, 'cum')
+    except:
+        print('Failed to send a message in send_start()')
 
 
+@repeat(every(15).minutes)
+def update_feed(listing):
+    if check_new_listings():
+        send_listing(listing)
+        PREVIOUS_LISTINGS.add(listing)
+
+@repeat(every(10).seconds)
+def test():
+    try:
+        bot.send_message(chat_id=config['CHANNEL'],text='cum')
+    except:
+        print('Failed to send a message test()')
+
+def check_new_listings():
+    NotImplemented
+
+def is_listed(listing):
+    return listing in PREVIOUS_LISTINGS
+
+def send_listing(listing : Listing):
+    try:        
+        bot.send_media_group(config['CHANNEL'],media=listing.photos_raw)
+    except:
+        print('Cant add images')
+        try:
+            bot.send_message(config['CHANNEL'],text=listing.compose())
+        except:
+            print('Cant send a message in send_listing()')
 
 if __name__ == '__main__':
     bot_polling = Thread(target=bot.infinity_polling)
--- page.py
+++ page.py
@@ -2,37 +2,46 @@ from requests import get
 from bs4 import BeautifulSoup
 from re import compile
 from dataclasses import dataclass
+from urllib.request import urlretrieve
 
 
 @dataclass
 class Listing:
     def __init__(self, href: str, logger=None) -> dict():
         self.url = href
-        title_regex = compile(r'<h1[^>]*>"(.*?)"</h1>')
+        title_regex = compile(r'<h1[^>]*>(.*?)</h1>')
         addr_regex = compile(r'<div[^>]*>([^<]+)</div>')
         prices_regex = compile(r'<span[^>]*>€ ([^<]+)</span>')
         area_rooms_regex = compile(r'<div[^>]*>\s*<span[^>]*>(\d+(?:,\d+)?)</span>')
+        photo_regex = compile(r'(src|data-flickity-lazyload)="([^"]*)"')
 
         soup = BeautifulSoup(get(href).text, 'html.parser')
 
         self.title = title_regex.search(str(soup.find('h1',
                                                       {'data-testid': 'ad-detail-header'}))).group(1)
         self.area = float(area_rooms_regex.search(str(soup.find('div',
-                                                          {'data-testid': 'ad-detail-teaser-attribute-0'}))).group(1))
+                                                          {'data-testid': 'ad-detail-teaser-attribute-0'}))).group(1).replace(",", ""))
         self.rooms = int(area_rooms_regex.search(str(soup.find('div',
                                                            {'data-testid': 'ad-detail-teaser-attribute-1'}))).group(1))
-        self.price = int(prices_regex.search(str(soup.find('span',
-                                                      {'data-testid': 'contact-box-price-box-price-value-0'}))).group(1))
-        self.kaution = int(prices_regex.search(str(soup.find('span',
-                                                        {'data-testid': 'price-information-freetext-attribute-value-1'}))).group(1))
+        self.price = float(prices_regex.search(str(soup.find('span',
+                                                      {'data-testid': 'contact-box-price-box-price-value-0'}))).group(1).replace(",", ""))
+        self.kaution = float(prices_regex.search(str(soup.find('span',
+                                                        {'data-testid': 'price-information-freetext-attribute-value-1'}))).group(1).replace(",", ""))
         self.address = addr_regex.search(str(soup.find('div',
                                                        {'data-testid': 'object-location-address'}))).group(1)
         
+        images = soup.find_all('button', {'class' : 'CarouselCell__InvisibleButton-sc-ouu5fe-0 gdmRaI'})
+        self.images_byte = set()
+        images_links = set()
+
+        for image in set(images):
+            images_links.add(photo_regex.search(str(image)).group(2))
+        print(images_links) 
         
-        # TODO images
         # TODO description
         # self.images_bytes = ...
         # self.description = ...
+        
         try:
             logger.info(self.__repr__())
         except:
@@ -42,11 +51,9 @@ class Listing:
         return (self.title == obj.title and self.url == obj.url)
 
 def get_photo(url):
-    req = get(url)
-    if req.status_code == 200:
-        return req.raw
-    else:
-        return None
+    photo = urlretrieve(url)
+    open('img.jpg','w').write(photo)
+    return photo
     
 
 def init_titles_file(file_path): 
@@ -55,3 +62,5 @@ def init_titles_file(file_path):
         for line in titles_file:
             titles.add(Listing(line))
     return titles
+
+print(Listing(r'https://www.willhaben.at/iad/immobilien/d/mietwohnungen/wien/wien-1180-waehring/tolle-2-zimmer-neubauwohnung-inkl-heizkosten-und-ww-621715992').images_byte)
