a64710f0e90515ac87da931fdb431f1af858780b
Stepan Denysenko <stepden04@gmail.com>
2023-05-09 01:32:08 +0300

    Added titles file initializer Changed Listings to dataclass

--- page.py
+++ page.py
@@ -1,58 +1,57 @@
 from requests import get
 from bs4 import BeautifulSoup
-import re
+from re import compile
+from dataclasses import dataclass
 
 
-def get_data_from_page(href: str, logger=None) -> dict():
-    """_summary_
+@dataclass
+class Listing:
+    def __init__(self, href: str, logger=None) -> dict():
+        self.url = href
+        title_regex = compile(r'<h1[^>]*>"(.*?)"</h1>')
+        addr_regex = compile(r'<div[^>]*>([^<]+)</div>')
+        prices_regex = compile(r'<span[^>]*>€ ([^<]+)</span>')
+        area_rooms_regex = compile(r'<div[^>]*>\s*<span[^>]*>(\d+(?:,\d+)?)</span>')
 
-    Args:
-        href (str, logger(optional)): _description_. Defaults to None)->dict(.
-        Link to a listing page
-    Returns:
-        Dictionary of title,price etc from a webpage
-    """
-    title_regex = re.compile(r'<h1[^>]*>"(.*?)"</h1>')
-    addr_regex = re.compile(r'<div[^>]*>([^<]+)</div>')
-    price_regex = re.compile(r'<span[^>]*>€ ([^<]+)</span>')
-    area_rooms_regex = re.compile(r'<div[^>]*>\s*<span[^>]*>(\d+(?:,\d+)?)</span>')
+        soup = BeautifulSoup(get(href).text, 'html.parser')
 
-    listing_dict = dict()
+        self.title = title_regex.search(str(soup.find('h1',
+                                                      {'data-testid': 'ad-detail-header'}))).group(1)
+        self.area = float(area_rooms_regex.search(str(soup.find('div',
+                                                          {'data-testid': 'ad-detail-teaser-attribute-0'}))).group(1))
+        self.rooms = int(area_rooms_regex.search(str(soup.find('div',
+                                                           {'data-testid': 'ad-detail-teaser-attribute-1'}))).group(1))
+        self.price = int(prices_regex.search(str(soup.find('span',
+                                                      {'data-testid': 'contact-box-price-box-price-value-0'}))).group(1))
+        self.kaution = int(prices_regex.search(str(soup.find('span',
+                                                        {'data-testid': 'price-information-freetext-attribute-value-1'}))).group(1))
+        self.address = addr_regex.search(str(soup.find('div',
+                                                       {'data-testid': 'object-location-address'}))).group(1)
+        
+        
+        # TODO images
+        # TODO description
+        # self.images_bytes = ...
+        # self.description = ...
+        try:
+            logger.info(self.__repr__())
+        except:
+            pass
+    
+    def __eq__(self, obj) -> bool:
+        return (self.title == obj.title and self.url == obj.url)
 
-    soup = BeautifulSoup(get(href).text, 'html.parser')
+def get_photo(url):
+    req = get(url)
+    if req.status_code == 200:
+        return req.raw
+    else:
+        return None
+    
 
-    # TODO add regex
-    title = title_regex.search(str(soup.find('h1',
-                          {'data-testid': 'ad-detail-header'}))).group(1)
-    area = area_rooms_regex.search(str(soup.find('div',
-                         {'data-testid': 'ad-detail-teaser-attribute-0'}))).group(1)
-    rooms = area_rooms_regex.search(str(soup.find('div',
-                          {'data-testid': 'ad-detail-teaser-attribute-1'}))).group(1)
-    price = price_regex.search(str(soup.find('span',
-                          {'data-testid': 'contact-box-price-box-price-value-0'}))).group(1)
-    kaution = price_regex.search(str(soup.find('span',
-                            {'data-testid': 'price-information-freetext-attribute-value-1'}))).group(1)
-    address = addr_regex.search(str(soup.find('div',
-                                {'data-testid': 'object-location-address'}))).group(1)
-    # imgs = set(...))
-
-    listing_dict.update(title=title, area=area, rooms=rooms,
-                        price=price, kaution=kaution, address=address)
-
-    try:
-        for val in listing_dict.values():
-           print(val, '\n')
-    except:
-        pass
-
-    return listing_dict
-
-# TODO images
-
-def get_images(links: set()):
-    images = set()
-
-    for link in links:
-        images.add(get(link))
-
-    return images
+def init_titles_file(file_path): 
+    titles = set()
+    with open(file_path,'a') as titles_file:
+        for line in titles_file:
+            titles.add(Listing(line))
+    return titles
